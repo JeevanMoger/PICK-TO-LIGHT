6028088036ea0d108d301a786a8ece55e8f5fcae
JeevanMoger <125747082+JeevanMoger@users.noreply.github.com>
2025-05-21 22:05:53 +0530

    Add files via upload

---
+++ PIC_TO _LIGHT_CLIENT1.X/build/default/production/main.p1.d
@@ -0,0 +1,7 @@
+build/default/production/main.p1:  \
+main.c  \
+main.h  \
+ssd_display.h  \
+digital_keypad.h  \
+eeprom.h  \
+can.h 
---
+++ PIC_TO _LIGHT_CLIENT1.X/build/default/production/ssd_fields.p1.d
@@ -0,0 +1,7 @@
+build/default/production/ssd_fields.p1:  \
+ssd_fields.c  \
+digital_keypad.h  \
+main.h  \
+ssd_display.h  \
+eeprom.h  \
+can.h 
