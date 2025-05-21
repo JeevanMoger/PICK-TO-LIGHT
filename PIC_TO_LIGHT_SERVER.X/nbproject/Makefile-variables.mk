8c195928d1da8d1f86ef532f50ac72bb8daa3d63
JeevanMoger <125747082+JeevanMoger@users.noreply.github.com>
2025-05-21 22:07:09 +0530

    Add files via upload

---
+++ PIC_TO_LIGHT_SERVER.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PIC_TO_LIGHT_SERVER.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PIC_TO_LIGHT_SERVER.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=pictolightserver.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/pictolightserver.x.tar
